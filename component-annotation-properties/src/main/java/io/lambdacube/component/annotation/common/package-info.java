4049458e1e1f512b07d84671040713783b40a5ba
Simon Chemouil <simon.chemouil@lambdacube.fr>
2016-05-05 00:40:39 +0200

    Build improvements

---
+++ component-annotation-properties/src/main/java/io/lambdacube/component/annotation/common/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * This package provides common component annotation properties.
+ */
+@Version("1.0")
+package io.lambdacube.component.annotation.common;
+
+import org.osgi.annotation.versioning.Version;
+
